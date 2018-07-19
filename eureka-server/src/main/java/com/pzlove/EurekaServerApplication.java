8dc7ad102a1f677786bd53734e0de711c4285f4b
mingjian.diao <mingjian.diao@ele.me>
2018-07-19 15:39:51 +0800

    init

---
+++ eureka-server/src/main/java/com/pzlove/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.pzlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心
+ *
+ * @author mingjian.diao
+ * @date 2018-06-06 下午2:32
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
